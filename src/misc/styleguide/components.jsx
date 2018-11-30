38a1b1c0d80baa33ae63472bb4a2b84fac8734f8
Sergo <rogaldh@radsh.red>
2018-11-30 12:50:15 +0300

    Add wrapper to output styled components

--- src/misc/styleguide/components.jsx
+++ src/misc/styleguide/components.jsx
@@ -7,9 +7,13 @@ import css from './components.css'
 
 const handleEnlarge = (e, props) => props.setEnlarged(!props.enlarged)
 
+const inner = props => !props.wrap
+  ? props.children
+  : <div className='inner'>{props.children}</div>
+
 const MakeFullscreen = props => (
   <div className={cn(css.root, { [css.enlarged]: props.enlarged })}>
-    {props.children}
+    {inner(props)}
     <button onClick={e => handleEnlarge(e, props)}>{!props.enlarged ? 'Enlarge' : 'Shrink'}</button>
   </div>
 )
