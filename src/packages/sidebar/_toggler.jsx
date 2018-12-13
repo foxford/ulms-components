d56e6c74da021437c4a4e912181534242e2c10a5
Sergo <rogaldh@radsh.red>
2018-12-13 16:34:36 +0300

    Sidebar (#7)
    * Simplify icons guide
    
    * Add `arrow` icon
    
    * Allow bypassing `className` to the panel
    
    * Increase `z-index` 4 the fullscreen wrapper
    
    * Add sidebar

---
+++ src/packages/sidebar/_toggler.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import cx from 'classnames'
+
+import { Arrow } from '../icons/icons'
+import css from '../sidebar/_toggler.css'
+
+export const Toggler = props => (
+  <div
+    className={cx(css.root, props.className, { [css.active]: props.toggled })}
+    onClick={props.handleClick}
+  >
+    {props.children || <Arrow />}
+  </div>
+)
