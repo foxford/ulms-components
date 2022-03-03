6273e7c8ecc515e2403b237609e318c895905cd0
Alex Matveev <ay.flyer@gmail.com>
2022-03-03 12:54:11 +0300

    fix: фикс "прострела" событий клавиатуры из чата на доску

--- packages/drawing/index.js
+++ packages/drawing/index.js
@@ -5,3 +5,5 @@ export { Drawing } from './src/drawing'
 export { LockProvider } from './src/lock-provider'
 
 export { CopyPasteProvider } from './src/copy-paste-provider'
+
+export { KeyboardListenerProvider, keyboardEvents } from './src/keyboard-listener-provider'
