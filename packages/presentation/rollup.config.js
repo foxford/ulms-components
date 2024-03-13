00000b85cf3de942c64c1e3385920f6afc59c92c
Alex Matveev <ay.flyer@gmail.com>
2024-03-13 17:48:48 +0300

    fix: css vars

--- packages/presentation/rollup.config.js
+++ packages/presentation/rollup.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 const cssdupl = require('postcss-discard-duplicates')
-const cssnext = require('postcss-cssnext')
+// const cssnext = require('postcss-cssnext')
 const cssurl = require('postcss-url')
 const Debug = require('debug')
 const env = require('postcss-preset-env')
@@ -46,7 +46,7 @@ const rollupPlugins = [ // order matters
     plugins: [
       cssurl({ url: 'inline' }),
       env(),
-      cssnext(),
+      // cssnext(),
       cssdupl()
     ].concat(shouldMinifyCss()),
     loaders: [
